fcbd0ea8fc2a622ca5faa472e343cce73f1c9fb2
Huldson Richard <huldson-_-@hotmail.com>
2021-04-14 03:31:50 -0300

    corrigido o erro estava dentro do if && e ||

--- anobi.js
+++ anobi.js
@@ -1,10 +1,10 @@
 function anosbis(ano) {
 
-if(ano%4==0 || ano%100!=0 && ano%400==0 ){
+if(ano%4==0 && ano%100!=0 || ano%400==0 ){
     console.log(ano+" esse ano é bissesto")
 }
 else {
     console.log(ano+" não é bissesto ")
 }   
 }
-anosbis(2003)
+anosbis(400)
