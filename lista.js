6d71c9c8b8552462ff14148dc411f7360f62fb58
Huldson Richard <huldson-_-@hotmail.com>
2021-04-15 03:36:12 -0300

    corrigido o bug de repetir os numeros

--- lista.js
+++ lista.js
@@ -2,17 +2,26 @@ function lista(numeros){
 let tamanho = numeros.length
 for(let i =0; i<numeros.length;i++){
     if(numeros[i]%3==0){
-    console.log(numeros[i]+"fizz")
-    }
-    if(numeros[i]%5==0){
-        console.log(numeros[i]+"buzz")
-    }
-    if(numeros[i]%3==0 && numeros[i]%5==0 ){
+        if(numeros[i]%3==0 && numeros[i]%5==0 ){
         console.log(numeros[i]+"fizz buzz")
-    }
+         }
+         else{
+            console.log(numeros[i]+"fizz")   
+         }
+
 }
 
-
+    else if(numeros[i]%5==0){
+        
+        if(numeros[i]%3==0 && numeros[i]%5==0 ){
+            console.log(numeros[i]+"fizz buzz")
+        } 
+         else{
+            console.log(numeros[i]+"buzz")
+        }        
+    }
+  
+}
 }
 
 let array=[3,2,5,6,7,8,9,15]
