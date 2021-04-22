3d6e9668c07ca4d0089d614b6ec2987c6a23ba92
Huldson Richard <huldson-_-@hotmail.com>
2021-04-22 05:50:26 -0300

    arrumando o codigo

--- teste.js
+++ teste.js
@@ -3,30 +3,26 @@ function timeConversion(s) {
     let hora = s[0]+s[1]
     let base = s[2]+s[3]+s[4]+s[5]+s[6]+s[7]
     hora=parseInt(hora)
-
+    
         if(s[8]=='A'){
             if(hora == 12){
             hora = "00"
-           
             return hora+base
-        
+           }
+            else{
+             return '0'+hora+base
+            }
         }
-        else
-        return '0'+hora+base
-    }
     else if(hora == 12){
         hora = "12"
-        
         return hora+base
     }
 
-        else{
+    else{
          hora=hora+12
          return hora+base
-        }
-  
-  
-  }
+    }
+}
  
  let v = "12:45:54PM"
 
