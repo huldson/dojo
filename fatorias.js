12c17e5fa3b3c22b9e8fec1f8ecc1f5048a9cf52
Huldson Richard <huldson-_-@hotmail.com>
2021-04-28 04:02:04 -0300

    o codigo fatorias está passando em 6/9 testes

---
+++ fatorias.js
@@ -0,0 +1,38 @@
+function getTotalX(a, b) {
+
+let tamanho=a.length
+let tamanho2=b.length
+let controle=0
+let comparador=0
+let somador = 0
+
+for(let i=0;i<tamanho;i++){
+    controle =0
+    for(let j=0;j<tamanho;j++){
+     if(a[i]%a[j]==0){
+        controle++
+     }   
+     else{
+
+     }
+      if(controle==tamanho){
+        comparador =a[i]
+      }  
+    }
+}
+if(comparador==0){
+    comparador=a[tamanho-1]
+}
+for(let i=0;i<tamanho2;i++){
+    if(b[i]%comparador==0){
+        somador++
+    }
+}
+
+return somador
+}
+let vari =[3,4]
+let vi =[24,48]
+
+
+console.log(getTotalX(vari,vi))
